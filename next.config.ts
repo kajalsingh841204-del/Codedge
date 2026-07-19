68f71f5e76a0cb1412903032d0c3e4daa0c036d2
kajalsingh841204-del <kajalsingh841204@gmail.com>
2026-07-19 18:35:50 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "codedgeacademy.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
 
